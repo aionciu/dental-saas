e585878ccd04575e7b717406665a8b1c1615348e
aionciu <ionciu.alexandru@gmail.com>
2025-09-06 23:42:16 +0300

    add Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 // src/app/layout.tsx
 import "@/app/globals.css";
-
+import { Analytics } from '@vercel/analytics/next';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-gray-50 text-gray-900">
         {children}
+        <Analytics />
       </body>
     </html>
   );
